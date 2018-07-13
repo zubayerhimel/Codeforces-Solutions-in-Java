327aeabf65a8be07a5be6b0993f75dda8bd8bbdf
Zubayer Himel <zhimel007@gmail.com>
2018-07-13 16:14:00 +0600

    Add files via upload

--- solution266A.java
+++ solution266A.java
@@ -1,13 +1,20 @@
-package codeforces.solutions;
+package DIU_ICPC;
 import java.util.Scanner;
-public class solution266A {
-    
-    public static void main(String[] args) {
-        
-        Scanner myscanner = new Scanner(System.in);
-        int limit = myscanner.nextInt();
-        String str = myscanner.next();
-        int a = 4;
-        
-    }
+public class Main_Stone {
+
+	public static void main(String[] args) {
+		Scanner myscanner = new Scanner(System.in);
+		int limit = myscanner.nextInt();
+		String str = myscanner.next();
+		int count=0;
+		for(int i=1; i<limit; i++)
+		{
+			if(str.charAt(i) == str.charAt(i-1))
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+		myscanner.close();
+	}
 }
